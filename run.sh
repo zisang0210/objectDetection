ec06207c2768d4896e987f94426f9f6535ea1ed1
zisang0210 <467862440@qq.com>
2018-01-12 19:13:54 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,14 +10,14 @@ export PYTHONPATH=$PYTHONPATH:$DIR:$DIR/slim:$DIR/object_detection
 
 # 定义各目录
 output_dir=/output  # 训练目录
-dataset_dir=/data/zisang0210/w8-data # 数据集目录，这里是写死的，记得修改
+dataset_dir=/data/zisang0210/week8-data # 数据集目录，这里是写死的，记得修改
 
 train_dir=$output_dir/train
 checkpoint_dir=$train_dir
 eval_dir=$output_dir/eval
 
 # config文件
-config=ssd_mobilenet_v1_pet.config
+config=ssd_mobilenet_v1_pets.config
 pipeline_config_path=$output_dir/$config
 
 # 先清空输出目录，本地运行会有效果，tinymind上运行这一行没有任何效果
